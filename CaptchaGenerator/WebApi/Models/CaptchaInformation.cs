e67bbe032213fa7140d70d0db4383438477c9230
maxneo4 <maxneo4@gmail.com>
2016-09-01 14:09:25 -0500

    Hello world for captcha info

---
+++ CaptchaGenerator/WebApi/Models/CaptchaInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebApi.Models
+{
+    public class CaptchaInformation
+    {
+        public const string QUESTION_TYPE_GREATER = "GREATER";
+        public const string QUESTION_TYPE_LESS = "LESS";
+
+        public Guid Id { get; set; }
+        public int[] Options { get; set; }
+        public string QuestionType { get; set; }
+        public string ImageBase64 { get; set; }
+    }
+}
