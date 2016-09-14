f5cfd1bf91a407413cf6b6d95250ac70ccdc66e1
maxneo4 <maxneo4@gmail.com>
2016-09-13 22:24:57 -0500

    Creando base de datos, agregando datos de conexion

--- CaptchaGenerator/Persistence/CaptchaPersistence.cs
+++ CaptchaGenerator/Persistence/CaptchaPersistence.cs
@@ -2,5 +2,14 @@
 {
     public class CaptchaPersistence
     {
+      /*
+      config = {
+          'user': 'vppdev_captcha',
+          'password': 'bizagi',
+          'host': 'stevie.heliohost.org',
+          'database': 'vppdev_captcha',
+          'raise_on_warnings': True
+      }
+      */
     }
 }
